34dab391d1a7e666c6db7567827ebf041ffb11bb
Seoho Choi <seohochoi1@gmail.com>
2022-03-16 12:11:01 -0700

    stage this

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -12,10 +12,11 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
-    will this push?
+
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
+    
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
